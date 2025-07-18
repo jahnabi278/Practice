0e67f8e4094b1e98d7bb124745f7110ce1f4e2a2
Jahnabi Sharma <jahnabi.s@terrapay.com>
2025-07-18 10:42:45 +0530

    updated

--- PracticeCode/src/main/java/com/terra/app/logical/ReverseString.java
+++ PracticeCode/src/main/java/com/terra/app/logical/ReverseString.java
@@ -2,13 +2,21 @@ package com.terra.app.logical;
 
 public class ReverseString {
 	public static void main(String[] args) {
-		String strToReverse = "Jahnabi";
+		String strToReverse = "Jahnabi Sharma";
 		char[] charArr = strToReverse.toCharArray();
 		StringBuilder sb = new StringBuilder();
+		// without using build-in method
 		for (int i = charArr.length - 1; i >= 0; i--) {
 			sb.append(charArr[i]);
 		}
 		System.out.println(sb.toString());
+		String reverseStr = "";
+		for (int i = charArr.length - 1; i >= 0; i--) {
+			reverseStr += charArr[i];
+		}
+		System.out.println(reverseStr);
+		// build-in function
+		System.out.println(new StringBuilder(strToReverse).reverse().toString());
 	}
 
 }
